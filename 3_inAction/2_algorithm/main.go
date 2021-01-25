62b36be279817b6131cb6b9fa9f3ed8ce079ddd1
unknown <656119364@qq.com>
2021-01-25 21:14:56 +0800

    commit algorithm

--- 3_inAction/2_algorithm/main.go
+++ 3_inAction/2_algorithm/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"Gogogo/3_inAction/2_algorithm/sort"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("Algorithm Test Start...")
+
+	nums := []int{61, 17, 29, 22, 34, 60, 72, 21, 50, 1, 62}
+	//sort.Sort_bubble(nums)
+	//sort.Sort_selection(nums)
+	sort.Sort_insertion(nums)
+	fmt.Println(nums)
 }
--- 3_inAction/2_algorithm/sort/sort.go
+++ 3_inAction/2_algorithm/sort/sort.go
@@ -1,5 +1,40 @@
 package sort
 
-func sort1() {
+// 冒泡排序
+func Sort_bubble(nums []int) {
+	for i := 0; i < len(nums) - 1; i++ {
+		for j := 0; j < len(nums) - i - 1; j++ {
+			if nums[j] > nums[j+1] {
+				nums[j], nums[j+1] = nums[j+1], nums[j]
+			}
+		}
+	}
+}
 
+// 选择排序
+func Sort_selection(nums []int) {
+	for i := 0; i < len(nums) - 1; i++ {
+		minIdx := i
+		for j := i; j < len(nums) - 1; j++ {
+			if nums[j] < nums[minIdx] {
+				minIdx = j
+			}
+		}
+
+		if i != minIdx {
+			nums[i], nums[minIdx] = nums[minIdx], nums[i]
+		}
+	}
+}
+
+// 插入排序
+func Sort_insertion(nums []int) {
+	for i := 0; i < len(nums) - 1; i++ {
+		for j := i + 1; j > 0; j-- {
+			if nums[j] >= nums[j-1] {
+				break
+			}
+			nums[j], nums[j-1] = nums[j-1], nums[j]
+		}
+	}
 }
