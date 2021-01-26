5ca44daa630313e47343b33c83dc1e84a72022ce
unknown <656119364@qq.com>
2021-01-26 21:06:51 +0800

    add shell_sort

--- 3_inAction/2_algorithm/main.go
+++ 3_inAction/2_algorithm/main.go
@@ -8,9 +8,10 @@ import (
 func main() {
 	fmt.Println("Algorithm Test Start...")
 
-	nums := []int{61, 17, 29, 22, 34, 60, 72, 21, 50, 1, 62}
+	nums := []int{13, 14, 94, 33, 82, 25, 59, 94, 65, 23, 45, 27, 73, 25, 39, 10}
 	//sort.Sort_bubble(nums)
 	//sort.Sort_selection(nums)
-	sort.Sort_insertion(nums)
-	fmt.Println(nums)
+	//sort.Sort_insertion(nums)
+	sort.Sort_shell(nums)
+	//fmt.Println(nums)
 }
--- 3_inAction/2_algorithm/sort/sort.go
+++ 3_inAction/2_algorithm/sort/sort.go
@@ -1,9 +1,13 @@
 package sort
 
+import (
+	"fmt"
+)
+
 // 冒泡排序
 func Sort_bubble(nums []int) {
-	for i := 0; i < len(nums) - 1; i++ {
-		for j := 0; j < len(nums) - i - 1; j++ {
+	for i := 0; i < len(nums)-1; i++ {
+		for j := 0; j < len(nums)-i-1; j++ {
 			if nums[j] > nums[j+1] {
 				nums[j], nums[j+1] = nums[j+1], nums[j]
 			}
@@ -13,9 +17,9 @@ func Sort_bubble(nums []int) {
 
 // 选择排序
 func Sort_selection(nums []int) {
-	for i := 0; i < len(nums) - 1; i++ {
+	for i := 0; i < len(nums)-1; i++ {
 		minIdx := i
-		for j := i; j < len(nums) - 1; j++ {
+		for j := i; j < len(nums)-1; j++ {
 			if nums[j] < nums[minIdx] {
 				minIdx = j
 			}
@@ -29,12 +33,45 @@ func Sort_selection(nums []int) {
 
 // 插入排序
 func Sort_insertion(nums []int) {
-	for i := 0; i < len(nums) - 1; i++ {
+	for i := 0; i < len(nums)-1; i++ {
 		for j := i + 1; j > 0; j-- {
 			if nums[j] >= nums[j-1] {
 				break
 			}
 			nums[j], nums[j-1] = nums[j-1], nums[j]
 		}
+	}
+}
+
+// 希尔排序
+func Sort_shell(nums []int) {
+	for stepLength := len(nums) / 2; stepLength >= 1; stepLength = stepLength / 2 {
+		fmt.Printf("==============================[StepLength: %d]==============================\n", stepLength)
+		for i := 0; i < len(nums); i++ {
+			fmt.Printf("%d   ", nums[i])
+			if (i+1)%stepLength == 0 {
+				fmt.Println()
+			}
+		}
+		fmt.Println()
+
+		for i := 0; i < stepLength; i++ {
+			for j := i; j+stepLength < len(nums); {
+				if nums[j] > nums[j+stepLength] {
+					nums[j+stepLength], nums[j] = nums[j], nums[j+stepLength]
+				} else {
+					j += stepLength
+				}
+			}
+		}
+
+		for i := 0; i < len(nums); i++ {
+			fmt.Printf("%d   ", nums[i])
+			if (i+1)%stepLength == 0 {
+				fmt.Println()
+			}
+		}
+		fmt.Println()
+		fmt.Println(nums)
 	}
 }
